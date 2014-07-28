fc0bde6dc2efd59bec8548a877281d5521c1d425
webchick <drupal@webchick.net>
2014-07-27 23:15:29 -0300

    Updating for latest Dev Desktop + clone from 8.x directly because living DANGEROUSLY IS FUN.

--- cmi.sh
+++ cmi.sh
@@ -25,19 +25,19 @@ cd prod
 
 # Grab a "shallow copy" of the latest D8 code, since we just want the files, not
 # the whole history. Then blow away its .git folder so ours doesn't conflict.
-#git clone --branch 8.x --depth 1 http://git.drupal.org/project/drupal.git dee-eight
-#rm -rf dee-eight/.git
+git clone --branch 8.x --depth 1 http://git.drupal.org/project/drupal.git dee-eight
+rm -rf dee-eight/.git
 
 # Move all of the D8 files into prod and destroy the temp folder.
 # @todo I could avoid all of this fuckery if git supported "svn export." :P
-#shopt -s dotglob
-#mv dee-eight/* .
-#rmdir dee-eight
+shopt -s dotglob
+mv dee-eight/* .
+rmdir dee-eight
 
 # Grab latest D8 release.
-wget http://ftp.drupal.org/files/projects/drupal-8.0-alpha9.tar.gz
-tar --strip-components=1 -zxvf drupal-8.0-alpha9.tar.gz
-rm drupal-8.0-alpha9.tar.gz
+#wget http://ftp.drupal.org/files/projects/drupal-8.0-alpha9.tar.gz
+#tar --strip-components=1 -zxvf drupal-8.0-alpha9.tar.gz
+#rm drupal-8.0-alpha9.tar.gz
 
 # Now populate the prod Git repo.
 git add .
@@ -51,8 +51,8 @@ git push origin master
 #git commit -am "Applying responsive preview patch."
 
 # Install Drupal 8: prod.
-mysql -e "DROP DATABASE IF EXISTS prod; CREATE DATABASE prod;" -uroot -h127.0.0.1 -P33066
-drush si --db-url=mysql://root:@127.0.0.1:33066/prod --account-pass=admin -y
+mysql -e "DROP DATABASE IF EXISTS prod; CREATE DATABASE prod;" -uroot -h127.0.0.1 -P33067
+drush si --db-url=mysql://root:@127.0.0.1:33067/prod --account-pass=admin -y
 
 # Hard-code site name to differentiate between sites. 
 echo "
@@ -74,8 +74,8 @@ git clone file:///Users/webchick/Sites/d8demo.git dev
 cd dev
 
 # Copy over the database.
-mysql -e "DROP DATABASE IF EXISTS dev; CREATE DATABASE dev;" -uroot -h127.0.0.1 -P33066
-mysqldump  -uroot -h127.0.0.1 -P33066 prod | mysql -uroot -h127.0.0.1 -P33066 dev
+mysql -e "DROP DATABASE IF EXISTS dev; CREATE DATABASE dev;" -uroot -h127.0.0.1 -P33067
+mysqldump  -uroot -h127.0.0.1 -P33067 prod | mysql -uroot -h127.0.0.1 -P33067 dev
 
 # Set the database/config directories for dev.
 cp ../prod/sites/default/settings.php sites/default
